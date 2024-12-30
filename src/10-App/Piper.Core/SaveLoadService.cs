6c527782c45f9be12e7a916756e70d8435946b71
Marco van den Oever <marco@flyingpie.nl>
2024-12-30 23:06:29 +0100

    Workspace

--- src/10-App/Piper.Core/SaveLoadService.cs
+++ src/10-App/Piper.Core/SaveLoadService.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.Json.Serialization;
 using Blazor.Diagrams;
 using Blazor.Diagrams.Core.Models;
+using Blazor.Diagrams.Core.Models.Base;
 using Newtonsoft.Json;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
@@ -12,7 +14,14 @@ public class SaveLoadService
 {
 	public void Load(BlazorDiagram diagram)
 	{
+		var jsonTxt = File.ReadAllText("/home/marco/Downloads/piper1.json");
+		var model = JsonConvert.DeserializeObject<PiperSave>(jsonTxt);
 
+		diagram.Groups.Clear();
+		diagram.Nodes.Clear();
+		diagram.Links.Clear();
+
+		model.ApplyTo(diagram);
 	}
 
 	public void Save(BlazorDiagram diagram)
@@ -20,12 +29,15 @@ public class SaveLoadService
 		var model = new PiperSave()
 		{
 			Nodes = diagram.Nodes.ToList(),
+			Links = diagram.Links.Select(l => ToLink(l)).Where(l => l != null).ToList(),
 		};
 
 		var json = JsonConvert.SerializeObject(model, new JsonSerializerSettings()
 		{
+			Formatting = Formatting.Indented,
 			PreserveReferencesHandling = PreserveReferencesHandling.All,
 			ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+			TypeNameHandling = TypeNameHandling.All,
 		});
 
 		// var json = JsonSerializer.Serialize(model, new()
@@ -33,7 +45,27 @@ public class SaveLoadService
 		//
 		// });
 
+		File.WriteAllText("/home/marco/Downloads/piper1.json", json);
+
 		var dbg = 2;
+	}
+
+	public static PiperSaveLink? ToLink(BaseLinkModel link)
+	{
+		var srcPort = link.Source.Model as PortModel;
+		var dstPort = link.Target.Model as PortModel;
+
+		if (srcPort == null || dstPort == null)
+		{
+			return null;
+		}
+
+		if (srcPort.Parent == null || dstPort.Parent == null)
+		{
+			return null;
+		}
+
+		return new PiperSaveLink() { Src = srcPort.Parent, Dst = dstPort.Parent, };
 	}
 }
 
@@ -41,5 +73,20 @@ public class PiperSave
 {
 	public List<NodeModel> Nodes { get; set; }
 
-	// public List<> Type { get; set; }
+	public List<PiperSaveLink> Links { get; set; }
+
+	public void ApplyTo(BlazorDiagram diagram)
+	{
+		foreach (var node in Nodes)
+		{
+			// diagram.Nodes.Add(
+		}
+	}
+}
+
+public class PiperSaveLink
+{
+	public NodeModel Src { get; set; }
+
+	public NodeModel Dst { get; set; }
 }
