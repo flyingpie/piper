435edb7f0f26acd5a5fd58dd5fc3f3266c54f20e
Marco van den Oever <marco@flyingpie.nl>
2025-09-04 22:52:27 +0200

    Workspace

--- src/10-App/Piper.UI/BlazorDiagramConfiguration.cs
+++ src/10-App/Piper.UI/BlazorDiagramConfiguration.cs
@@ -45,35 +45,35 @@ public static class BlazorDiagramConfiguration
 			new NodeModel(position: new Point(200, 100)) { Title = "Node 2" }
 		);
 
-		var catNode = diagram.Nodes.Add(
-			new ListFilesNodeModel()
-			{
-				Position = new Point(50, 200),
-				Title = "Node 3",
-				Command = "cat",
-				Args =
-				[
-					new ListFilesNodeModel.CmdArgument()
-					{
-						Arg = "/home/marco/Downloads/jsonnd.txt",
-					},
-				],
-			}
-		);
-
-		var jqNode = diagram.Nodes.Add(
-			new ListFilesNodeModel()
-			{
-				Position = new Point(400, 200),
-				Title = "Node 3",
-				Command = "jq",
-				Args = [new ListFilesNodeModel.CmdArgument() { Arg = "-c" }],
-			}
-		);
-
-		var src = new SinglePortAnchor(catNode.StdOut);
-		var dst = new SinglePortAnchor(jqNode.StdIn);
-		var link1 = diagram.Links.Add(new LinkModel(src, dst));
+		// var catNode = diagram.Nodes.Add(
+		// 	new ListFilesNodeModel()
+		// 	{
+		// 		Position = new System.Drawing.Point(50, 200),
+		// 		Name = "Node 3",
+		// 		Command = "cat",
+		// 		Args =
+		// 		[
+		// 			new ListFilesNodeModel.CmdArgument()
+		// 			{
+		// 				Arg = "/home/marco/Downloads/jsonnd.txt",
+		// 			},
+		// 		],
+		// 	}
+		// );
+		//
+		// var jqNode = diagram.Nodes.Add(
+		// 	new ListFilesNodeModel()
+		// 	{
+		// 		Position = new System.Drawing.Point(400, 200),
+		// 		Name = "Node 3",
+		// 		Command = "jq",
+		// 		Args = [new ListFilesNodeModel.CmdArgument() { Arg = "-c" }],
+		// 	}
+		// );
+		//
+		// var src = new SinglePortAnchor(catNode.StdOut);
+		// var dst = new SinglePortAnchor(jqNode.StdIn);
+		// var link1 = diagram.Links.Add(new LinkModel(src, dst));
 
 		var p1 = firstNode.AddPort(PortAlignment.Right);
 		var p2 = firstNode.AddPort(PortAlignment.Right);
